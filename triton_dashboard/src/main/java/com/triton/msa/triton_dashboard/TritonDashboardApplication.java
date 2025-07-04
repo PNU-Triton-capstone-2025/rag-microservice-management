cb4f4f4bc71307ee922cade0df13239eef6d1fbb
sul1074 <sul107485@gmail.com>
2025-07-04 13:48:58 +0900

    feat/base file init

---
+++ triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/TritonDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.triton.msa.triton_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TritonDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TritonDashboardApplication.class, args);
+	}
+
+}

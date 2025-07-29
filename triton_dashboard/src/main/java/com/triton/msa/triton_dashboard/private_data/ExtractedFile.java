cc9b52aa838e61ae820f4164479f8febf292fbc1
sul1074 <sul107485@gmail.com>
2025-07-29 15:57:42 +0900

    feat-24/UI 개선 #24

--- triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/private_data/ExtractedFile.java
+++ triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/private_data/ExtractedFile.java
@@ -2,7 +2,5 @@ package com.triton.msa.triton_dashboard.private_data;
 
 import java.time.Instant;
 
-public record ExtractedFile(String filename, String content, Instant timestamp) {
-
-}
+public record ExtractedFile(String filename, String content, Instant timestamp) {}
 

6dff6db459fac293c1de9995c912161166469546
sinsehwan <sk124590@gmail.com>
2025-08-06 17:32:28 +0900

    feat(config): implement ssh websocket configuration #43

---
+++ triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/ssh/handler/SshWebSocketHandler.java
@@ -0,0 +1,8 @@
+package com.triton.msa.triton_dashboard.ssh.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class SshWebSocketHandler extends TextWebSocketHandler {
+}

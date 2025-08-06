2cc834016e591ed342901615d4dc62f812b2cc06
sinsehwan <sk124590@gmail.com>
2025-08-06 19:44:42 +0900

    feat(ssh): implement simple sshController

--- triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/ssh/controller/SshController.java
+++ triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/ssh/controller/SshController.java
@@ -1,34 +1,19 @@
 package com.triton.msa.triton_dashboard.ssh.controller;
 
-import com.triton.msa.triton_dashboard.project.entity.Project;
-import com.triton.msa.triton_dashboard.project.repository.ProjectRepository;
-import com.triton.msa.triton_dashboard.project.service.ProjectService;
-import com.triton.msa.triton_dashboard.ssh.service.SshService;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
-@RestController
-@RequestMapping("/api/ssh")
+@Controller
+@RequestMapping("/ssh")
 public class SshController {
-    private final SshService sshService;
-    private final ProjectService projectService;
 
-    public SshController(SshService sshService, ProjectService projectService) {
-        this.sshService = sshService;
-        this.projectService = projectService;
-    }
+    @GetMapping("/connect/{projectId}")
+    public String sshConnectUI(@PathVariable String projectId, Model model) {
+        model.addAttribute("projectId", projectId);
 
-    @PostMapping("/connect/{projectId}")
-    public ResponseEntity<Map<String, String>> initiateSshConnection(@PathVariable Long projectId) {
-        Project project = projectService.getProject(projectId);
-
-        String sessionId = sshService.startSshSession(project.getSshInfo());
-
-        return ResponseEntity.ok(Map.of("sessionID", sessionId));
+        return "ssh/ssh-client";
     }
 }

2c48fdcb2d3a8b5cb28668ae55841fdbbd8eeeca
sinsehwan <sk124590@gmail.com>
2025-08-16 16:35:36 +0900

    chore(dashboard): apply jsonProperties in all dto #90

--- triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/rag/dto/RagResponseDto.java
+++ triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/rag/dto/RagResponseDto.java
@@ -1,6 +1,9 @@
 package com.triton.msa.triton_dashboard.rag.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record RagResponseDto(
+        @JsonProperty("user_query")
         String userQuery,
         String response
 ) {
--- triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/rag_history/dto/RagHistoryResponseDto.java
+++ triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/rag_history/dto/RagHistoryResponseDto.java
@@ -1,13 +1,17 @@
 package com.triton.msa.triton_dashboard.rag_history.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.triton.msa.triton_dashboard.rag_history.entity.RagHistory;
 
 import java.time.LocalDateTime;
 
 public record RagHistoryResponseDto(
     Long id,
+    @JsonProperty("user_query")
     String UserQuery,
+    @JsonProperty("llm_response")
     String llmResponse,
+    @JsonProperty("created_at")
     LocalDateTime createdAt
 ) {
     public static RagHistoryResponseDto from(RagHistory entity) {

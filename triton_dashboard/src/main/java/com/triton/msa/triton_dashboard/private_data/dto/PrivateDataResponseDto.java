b762ffb07a1302ea8341c55b572b2d65d9fcbe81
sinsehwan <sk124590@gmail.com>
2025-08-16 16:28:27 +0900

    chore(private-data): apply jsonProperties in private-data dto #90

--- triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/private_data/dto/PrivateDataResponseDto.java
+++ triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/private_data/dto/PrivateDataResponseDto.java
@@ -1,12 +1,17 @@
 package com.triton.msa.triton_dashboard.private_data.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.Instant;
 
 public record PrivateDataResponseDto(
         Long id,
+        @JsonProperty("project_id")
         Long projectId,
         String filename,
+        @JsonProperty("content_type")
         String contentType,
+        @JsonProperty("created_at")
         Instant createdAt
 ) {
     public static PrivateDataResponseDto from(ProjectPrivateDataDto dto) {

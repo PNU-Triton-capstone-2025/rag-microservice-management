9b033ba33cd06f224dd91de5533ed343873e054b
sinsehwan <sk124590@gmail.com>
2025-08-24 17:46:34 +0900

    feat(docs): add api specification for monitoring #103

--- triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/monitoring/dto/RagLogRequestDto.java
+++ triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/monitoring/dto/RagLogRequestDto.java
@@ -1,6 +1,9 @@
 package com.triton.msa.triton_dashboard.monitoring.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record RagLogRequestDto(
+        @JsonProperty("es_index")
         String esIndex,
         String provider,
         String model,

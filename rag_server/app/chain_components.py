55fc3da1c0cae6ad866f357f199f4d6cbf5b9b29
sul1074 <sul107485@gmail.com>
2025-08-28 14:56:10 +0900

    fix/113-RAG 코드 최신화 close #113

--- rag_server/app/chain_components.py
+++ rag_server/app/chain_components.py
@@ -1,40 +1,34 @@
-import os
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_anthropic import ChatAnthropic
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_elasticsearch import ElasticsearchStore
-from langchain.prompts import PromptTemplate
-from prompt_template import DEFAULT_PROMPT_TEMPLATE, RESOURCE_PROMPT_TEMPLATE, LOG_PROMPT_TEMPLATE
+from settings import settings, Provider
+from prompt_template import prompts
 
-# --- Embedding ---
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatAnthropic
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from langchain_community.vectorstores import ElasticsearchStore
+
 def get_embedding_model():
     return OpenAIEmbeddings()
+    
+def get_chat_llm(provider, llm):
+    if provider == Provider.openai.value:
+        return ChatOpenAI(model_name = llm)
 
-# --- LLMs ---
-def get_chat_llm(provider: str, llm: str):
-    if provider == "openai":
-        return ChatOpenAI(model_name=llm, streaming=True, temperature=0)
-    elif provider == "anthropic":
-        return ChatAnthropic(model_name=llm, streaming=True, temperature=0)
-    elif provider == "gemini":
-        return ChatGoogleGenerativeAI(model=llm, streaming=True, temperature=0)
-    else:
-        raise ValueError(f"Unsupported LLM provider: {provider}")
+    if provider == Provider.anthropic.value:
+        return ChatAnthropic(model = llm)
 
-# --- Vector Store ---
-def get_vectorstore(index_name: str, embedding_model):
-    return ElasticsearchStore(
-        es_url=os.environ.get("ELASTICSEARCH_URL"),
+    if provider == Provider.gemini.value:
+        return ChatGoogleGenerativeAI(model = llm)
+    
+def get_vectorstore(index_name, embedding_model):
+    vectorstore = ElasticsearchStore(
+        es_url=settings.elasticsearch_url,
         index_name=index_name,
         embedding=embedding_model
     )
+    return vectorstore
 
-# --- Prompt Templates ---
-def get_prompt_tmpl(query_type: str):
-    if query_type == "resource":
-        template = RESOURCE_PROMPT_TEMPLATE
-    elif query_type == "log":
-        template = LOG_PROMPT_TEMPLATE
-    else: # default
-        template = DEFAULT_PROMPT_TEMPLATE
-    return PromptTemplate.from_template(template)
+def get_prompt_tmpl(query_type):
+    prompt_tmpl = prompts.get(query_type)
+    return prompt_tmpl

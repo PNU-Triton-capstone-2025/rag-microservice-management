8eda627e15ff758ba4aeeaa5d421f4da27f022a3
sul1074 <sul107485@gmail.com>
2025-09-01 20:05:06 +0900

    모니í터링 페이지에 ã모니터링 이력 조회 UI 추가 #134

--- triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/monitoring/controller/MonitoringController.java
+++ triton_dashboard/src/main/java/com/triton/msa/triton_dashboard/monitoring/controller/MonitoringController.java
@@ -2,11 +2,17 @@ package com.triton.msa.triton_dashboard.monitoring.controller;
 
 import com.triton.msa.triton_dashboard.monitoring.dto.SavedYamlRequestDto;
 import com.triton.msa.triton_dashboard.monitoring.dto.SavedYamlResponseDto;
+import com.triton.msa.triton_dashboard.monitoring.entity.MonitoringHistory;
+import com.triton.msa.triton_dashboard.monitoring.service.MonitoringHistoryService;
 import com.triton.msa.triton_dashboard.monitoring.service.MonitoringService;
 import com.triton.msa.triton_dashboard.project.dto.ProjectResponseDto;
 import com.triton.msa.triton_dashboard.project.entity.Project;
 import com.triton.msa.triton_dashboard.project.service.ProjectService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,21 +27,27 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/projects/{projectId}/monitoring")
 public class MonitoringController {
 
     private final MonitoringService monitoringService;
+    private final MonitoringHistoryService monitoringHistoryService;
     private final ProjectService projectService;
 
     @GetMapping
-    public String monitoringPage(@PathVariable("projectId") Long projectId, Model model) {
-        ProjectResponseDto project = ProjectResponseDto.from(projectService.getProject(projectId));
+    public String monitoringPage(@PathVariable("projectId") Long projectId,
+                                 @PageableDefault(size = 5, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+                                 Model model) {
+        ProjectResponseDto projectDto = ProjectResponseDto.from(projectService.getProject(projectId));
         List<SavedYamlResponseDto> savedYamls = monitoringService.getSavedYamls(projectId);
+        Page<MonitoringHistory> monitoringHistories = monitoringHistoryService.getMonitoringHistories(projectId, pageable);
 
-        model.addAttribute("project", project);
+        model.addAttribute("project", projectDto);
         model.addAttribute("savedYamls", savedYamls);
+        model.addAttribute("monitoringHistories", monitoringHistories);
 
         return "projects/monitoring";
     }
